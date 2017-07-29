0be2b2ff6fe013d46cc3457d2c9ccd57d23914a7
Michael Lee <nfreakct@gmail.com>
2017-07-29 03:49:52 -0400

    Stub for generator UI

---
+++ source/com/ludoko/ld39/ui/GeneratorUI.hx
@@ -0,0 +1,22 @@
+package com.ludoko.ld39.ui;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.group.FlxGroup;
+import flixel.text.FlxText;
+
+/**
+ * The UI panel for the generator.
+ * 
+ * @author Michael Lee
+ */
+class GeneratorUI extends FlxGroup
+{
+
+	public function new() 
+	{
+		super();
+		
+	}
+	
+}
